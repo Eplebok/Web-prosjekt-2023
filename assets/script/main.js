f93e23c32a79819e018a28ae5e173d1a098dc47a
sondbj <sondbj@stud.ntnu.no>
2023-02-09 18:03:27 +0100

    added footer and nav as web component

--- assets/script/main.js
+++ assets/script/main.js
@@ -49,7 +49,60 @@ function hideMenu(){
         }
 
        
+class FooterComponent extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+    <footer>
+    <div class="footer-container">
+        <div class="footer-top">
+            <p class="footer-top-info">Kontakt Info</p>
+                <ul>
+                    <li><a href="https://www.ntnu.no/" class="footer-list">NTNU Homepage</a></li>
+                    <li><a href="email@test.no" class="footer-list">NTNU mail</a></li>
+                    <li><a href="https://www.ntnu.no/ansatte/ida.parelius" class="footer-list">Person responsible for the workshop</a></li>
+                    <li><a href="email@test.no" class="footer-list">Idas mail</a></li>
+                    <li><a href="73595000" class="footer-list">Telefon sentralbord: 73 59 50 00</a></li>
+                </ul>
+        </div>
 
+        <div class="footer-top">
+            <p class="footer-top-info">Andre medier</p>
+                <ul>
+                    <li><a href="https://www.instagram.com/" class="fa fa-instagram"></a></li>
+                    <li><a href="https://www.facebook.com/" class="fa fa-facebook"></a></li>
+                </ul>
+        </div>
+    </div>
+    </footer>
+    `;
+  }
+}
+
+customElements.define('footer-component', FooterComponent);
+
+class NavComponent extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `
+        <header class="header">
+        <nav tabindex="0">
+           <div class="nav_links" id="nav_links">
+                
+              <ul role="navigation">
+                  <li id="leftlink" ><a id="test" href="index.html">Home</a></li>
+                  <li><a href="tools.html">Tools</a></li>
+                  <li><a href="booking.html">Booking</a></li>
+                  <li><a href="about.html">About Us</a></li>
+                  <li><a href="feedback.html">Feedback</a></li>
+                  <li><a href="login.html">Log In</a></li>         
+              </ul>
+          </div>  
+        </nav>
+        </header>
+      `;
+    }
+  }
+  
+  customElements.define('nav-component', NavComponent);
 
 
 
