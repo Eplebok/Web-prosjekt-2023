ba3b1bcb38365dc6440aac6365f9bc22e2a8624f
Eplebok <mathiakk@stud.ntnu.no>
2023-05-01 17:24:25 +0200

    added more stuff admin can do and changed index

--- assets/script/main.js
+++ assets/script/main.js
@@ -245,6 +245,84 @@ window.addEventListener('load', async () => {
   });
 
 
+  // for admin page
+  window.addEventListener('load', async () => {
+    try {
+      const response = await fetch('http://localhost:3200/all', {
+        method: "GET"
+      });
+      const allUsers = await response.json();
+      const ul = document.querySelector(".admin-all-user-list");
+      allUsers.forEach(user => {
+        const li = document.createElement('li');
+        const button = document.createElement("button");
+        button.innerHTML = 'Make this user admin';
+        li.innerHTML = `Email: ${user.email}. Role: ${user.role}`;
+        button.addEventListener('click', async () => {
+          try {
+            const updateUser = await fetch(`http://localhost:3200/all/${user._id}`, {
+              method: 'PUT',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({ role: 'admin' })
+            });
+            const updatedUser = await updateUser.json();
+            li.innerHTML = `Email: ${updatedUser.email}. Role: ${updatedUser.role}`;
+          } catch (error) {
+            console.log(error.message);
+          }
+        });
+        const makerButton = document.createElement("button")
+        makerButton.innerHTML = "Make this user an maker"
+        makerButton.addEventListener('click', async() => {
+          try {
+            const updateUser = await fetch(`http://localhost:3200/all/${user._id}`, {
+              method: 'PUT',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({ role: 'maker' })
+            });
+            const updatedUser = await updateUser.json();
+            li.innerHTML = `Email: ${updatedUser.email}. Role: ${updatedUser.role}`;
+          } catch (error) {
+            console.log(error.message);
+          }
+        })
+        const div = document.createElement("div")
+        div.append(button, makerButton)
+        li.append(div)
+        ul.append(li);
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
+  
+
+// for index.html page to display all the tools avaliable and to display the broken/functional tools
+window.addEventListener('load', async () => {
+  try {
+    const response = await fetch('http://localhost:3200/tools/tools');
+    const tools = await response.json();
+
+    const totalTools = tools.length;
+    const workingTools = tools.filter(tool => tool.functional === 'working').length;
+    const brokenTools = tools.filter(tool => tool.functional === 'broken').length;
+
+    const statsList = document.querySelector('.important-booking-list');
+    statsList.innerHTML = `
+      <li>Total tools: ${totalTools}</li>
+      <li>Available tools: ${workingTools}</li>
+      <li>Broken tools: ${brokenTools}</li>
+    `;
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+
 
 
 
